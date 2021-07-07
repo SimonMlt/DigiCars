5038475e583e8856b977f0247c44c69df66b10a6
Jeevesen Curpen <jorvencurpen92@gmail.com>
2021-07-07 20:46:13 +0200

    styles

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,12 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('homeV1');
+Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('homeV1');
+Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('homeV1');
+Route::get('/home', 'HomeController@index')->name('home');
