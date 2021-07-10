00a60ceea7e8b407b8fb94e6ff29eff8bc3c533d
Jeevesen <jorvencurpen92@gmail.com>
2021-07-10 19:31:47 +0200

    Modifs Réservation V3

--- resources/views/reservationdates.blade.php
+++ resources/views/reservationdates.blade.php
@@ -184,7 +184,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+{{--        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">--}}
         <style>
             @media only screen and (max-width: 760px),
             (min-device-width: 802px) and (max-device-width: 1020px) {
