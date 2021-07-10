581fd6f0c1505437ccd41be59520e16cf17b5869
Jeevesen <jorvencurpen92@gmail.com>
2021-07-10 17:10:01 +0200

    Pages Réservation + Table

---
+++ database/migrations/2021_07_10_145250_create_reservation_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservation', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('motif');
+            $table->date('date');
+            $table->string('timeslot');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservation');
+    }
+}
