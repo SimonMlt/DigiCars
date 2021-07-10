2118b99d42d88fa229e29cff707fd7918d900e48
Jeevesen <jorvencurpen92@gmail.com>
2021-07-10 16:01:46 +0200

    Back vehicules

--- resources/views/vehicules.blade.php
+++ resources/views/vehicules.blade.php
@@ -9,7 +9,6 @@
                                 <div class="card-body">
                                     <h5 class="card-title text-center">{{ $vehicule->marque }}</h5>
                                     <p class="card-text">{{ $vehicule->modele }}</p>
-                                    <a class="btn btn-primary float-right mt-4" href="{{ url('vehicules/edit/'.$vehicule->id) }}" >Modifier</a>
                                 </div>
                             </div>
                         </div>
