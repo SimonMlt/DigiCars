a3085d5b0cfcaa1bb354ef087455b577142a182e
Jeevesen Curpen <jorvencurpen92@gmail.com>
2021-07-13 18:02:14 +0200

    name reservation

--- resources/views/reservationheures.blade.php
+++ resources/views/reservationheures.blade.php
@@ -140,7 +140,7 @@ function timeslots($duration,$cleanup, $start, $end){
                             </div>
                             <div class="form-group">
                                 <label for="">Nom</label>
-                                <input required type="text" class="form-control" name="name">
+                                <input readonly type="text" class="form-control" name="name" value="{{ Auth::user()->name }}">
                             </div>
                             <div class="form-group">
                                 <label for="">Email</label>
@@ -162,8 +162,8 @@ function timeslots($duration,$cleanup, $start, $end){
 
     </div>
 </div>
-{{--<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>--}}
-{{--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>--}}
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 {{--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>--}}
 <script>
     $(".book").click(function(){
