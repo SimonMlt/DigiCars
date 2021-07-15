0e08ca4cfc8342720ffe1ac4c169f316b4bec757
Jeevesen <jorvencurpen92@gmail.com>
2021-07-15 22:08:16 +0200

    prix devis

--- resources/views/Back/createdevis.blade.php
+++ resources/views/Back/createdevis.blade.php
@@ -7,27 +7,38 @@
                 <div class="row" id="tabs">
                     <div class="col-lg-8">
                         <label>Client</label>
-                        <select name="id_client">
+                        <select class="form-control" name="id_client">
+                            <option selected disabled value="#"> --Sélectionner-- </option>
                             @foreach($users as $user)
                                 <option value="{{ $user->id }}">{{ $user->name }}</option>
                             @endforeach
                         </select>
 
                         <label>Véhicule</label>
-                        <select name="id_vehicule">
+                        <select class="form-control vehicle-type" name="id_vehicule">
+                            <option selected disabled value="#"> --Sélectionner-- </option>
                             @foreach($vehicules as $vehicule)
-                                <option value="{{ $vehicule->id }}">{{ $vehicule->marque }} {{ $vehicule->modele }}</option>
+                                <option data-price="{{ $vehicule->prix }}" value="{{ $vehicule->id }}">{{ $vehicule->marque }} {{ $vehicule->modele }}</option>
                             @endforeach
                         </select>
 
                         <label>Quantité</label>
-                        <input type="number" name="quantite">
+                        <input type="number" class="form-control" name="quantite">
 
-                        <label>Total</label>
-                        <input type="number" name="total">
+                        <label for="total">Total</label>
+                        <input type="text" name="total" class="form-control price-input" readonly>
                     </div>
                 </div>
                 <button type="submit" class="btn btn-primary float-right">Créer le devis</button>
             </form>
         </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script>
+            $('.vehicle-type').on('change', function() {
+                $('.price-input')
+                    .val(
+                        $(this).find(':selected').data('price')
+                    );
+            });
+        </script>
 @endsection
