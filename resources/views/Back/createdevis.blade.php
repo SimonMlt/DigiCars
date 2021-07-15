b50bfbac6e0be6dc357ca35a405cacc607980ca2
SimonMlt <simon-malet@hotmail.fr>
2021-07-15 22:08:46 +0200

    bug createdevis

--- resources/views/Back/createdevis.blade.php
+++ resources/views/Back/createdevis.blade.php
@@ -1,33 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
-        <div class="container" style="padding-top: 100px">
-            <form method="POST" action="{{ url('admin/devis/create') }}">
-                @csrf
-                <div class="row" id="tabs">
-                    <div class="col-lg-8">
-                        <label>Client</label>
-                        <select name="id_client">
-                            @foreach($users as $user)
-                                <option value="{{ $user->id }}">{{ $user->name }}</option>
-                            @endforeach
-                        </select>
+    <div class="container" style="padding-top: 100px">
+        <form method="POST" action="{{ url('admin/devis/create') }}">
+            @csrf
+            <div class="row" id="tabs">
+                <div class="col-lg-8">
+                    <label>Client</label>
+                    <select name="id_client">
+                        @foreach($users as $user)
+                            <option value="{{ $user->id }}">{{ $user->name }}</option>
+                        @endforeach
+                    </select>
 
-                        <label>Véhicule</label>
-                        <select name="id_vehicule">
-                            @foreach($vehicules as $vehicule)
-                                <option value="{{ $vehicule->id }}">{{ $vehicule->marque }} {{ $vehicule->modele }}</option>
-                            @endforeach
-                        </select>
+                    <label>Véhicule</label>
+                    <select name="id_vehicule" id="id_vehicule">
+                        @foreach($vehicules as $vehicule)
+                            <option value="{{ $vehicule->id }}">{{ $vehicule->marque }} {{ $vehicule->modele }}</option>
+                        @endforeach
+                    </select>
 
-                        <label>Quantité</label>
-                        <input type="number" name="quantite">
+                    <input type="text" id="prix" value="{{ $vehicule->prix }}">
 
-                        <label>Total</label>
-                        <input type="number" name="total">
-                    </div>
+                    <label>Quantité</label>
+                    <input type="number" name="quantite" id="quantite">
+
+                    <label>Total</label>
+                    <input type="text" name="total" id="total" disabled="disabled" readonly value="{{ $vehicule->prix }}">
                 </div>
-                <button type="submit" class="btn btn-primary float-right">Créer le devis</button>
-            </form>
-        </div>
+            </div>
+            <button type="submit" class="btn btn-primary float-right">Créer le devis</button>
+        </form>
+    </div>
+
+    <script>
+        const prix = document.querySelector("#prix");
+        const quantite = document.querySelector("#quantite");
+        const total = document.querySelector("#total");
+        const modele = document.querySelector("#id_vehicule");
+
+        quantite.addEventListener('change', (event) => {
+            total.value = quantite.value * prix.value;
+            console.log(prix);
+        })
+
+        modele.addEventListener('change', (event) => {
+            total.value = quantite.value * prix.value;
+        })
+    </script>
 @endsection
