632c4767356bf223a9eb7f0b790a6b6b369e5795
SimonMlt <simon-malet@hotmail.fr>
2021-07-15 15:18:16 +0200

    création devis via select et liste devis

--- resources/views/Back/createdevis.blade.php
+++ resources/views/Back/createdevis.blade.php
@@ -7,41 +7,26 @@
                 <div class="row" id="tabs">
                     <div class="col-lg-8">
                         <label>Client</label>
-                        <input list="id_clients" name="id_clients" placeholder="Chercher un client ...">
-                        <datalist id="id_clients">
+                        <select name="id_client">
                             @foreach($users as $user)
                                 <option value="{{ $user->id }}">{{ $user->name }}</option>
                             @endforeach
-                        </datalist>
-                    </div>
-                </div>
+                        </select>
 
-                <div class="row" id="tabs">
-                    <div class="col-lg-8">
                         <label>Véhicule</label>
-                        <input list="id_vehicule" name="id_vehicule" placeholder="Chercher un véhicule ...">
-                        <datalist id="id_vehicule">
+                        <select name="id_vehicule">
                             @foreach($vehicules as $vehicule)
                                 <option value="{{ $vehicule->id }}">{{ $vehicule->marque }} {{ $vehicule->modele }}</option>
                             @endforeach
-                        </datalist>
-                    </div>
-                </div>
+                        </select>
 
-                <div class="row" id="tabs">
-                    <div class="col-lg-8">
                         <label>Quantité</label>
                         <input type="number" name="quantite">
-                    </div>
-                </div>
 
-                <div class="row" id="tabs">
-                    <div class="col-lg-8">
                         <label>Total</label>
-                        <input type="number" name="total" step="0.01">
+                        <input type="number" name="total">
                     </div>
                 </div>
-
                 <button type="submit" class="btn btn-primary float-right">Créer le devis</button>
             </form>
         </div>
