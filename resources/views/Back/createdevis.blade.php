8431cbf2098ffea2bd0bb3ffb9e532b9edcd67ef
SimonMlt <simon-malet@hotmail.fr>
2021-07-15 14:48:37 +0200

    autocomplete création devis

--- resources/views/Back/createdevis.blade.php
+++ resources/views/Back/createdevis.blade.php
@@ -7,26 +7,41 @@
                 <div class="row" id="tabs">
                     <div class="col-lg-8">
                         <label>Client</label>
-                        <select name="id_client">
+                        <input list="id_clients" name="id_clients" placeholder="Chercher un client ...">
+                        <datalist id="id_clients">
                             @foreach($users as $user)
                                 <option value="{{ $user->id }}">{{ $user->name }}</option>
                             @endforeach
-                        </select>
+                        </datalist>
+                    </div>
+                </div>
 
+                <div class="row" id="tabs">
+                    <div class="col-lg-8">
                         <label>Véhicule</label>
-                        <select name="id_vehicule">
+                        <input list="id_vehicule" name="id_vehicule" placeholder="Chercher un véhicule ...">
+                        <datalist id="id_vehicule">
                             @foreach($vehicules as $vehicule)
                                 <option value="{{ $vehicule->id }}">{{ $vehicule->marque }} {{ $vehicule->modele }}</option>
                             @endforeach
-                        </select>
-
-                        <label>Quantité</label>
-                        <input type="number" name="quantite">
-
-                        <label>Total</label>
-                        <input type="number" name="total">
+                        </datalist>
                     </div>
                 </div>
+
+                <div class="row" id="tabs">
+                    <div class="col-lg-8">
+                        <label>Quantité</label>
+                        <input type="number" name="quantite">
+                    </div>
+                </div>
+
+                <div class="row" id="tabs">
+                    <div class="col-lg-8">
+                        <label>Total</label>
+                        <input type="number" name="total" step="0.01">
+                    </div>
+                </div>
+
                 <button type="submit" class="btn btn-primary float-right">Créer le devis</button>
             </form>
         </div>
