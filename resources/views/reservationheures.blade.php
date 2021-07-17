f66d2cbdce37f93b024d83fa82b47da9da0dc991
Jeevesen <jorvencurpen92@gmail.com>
2021-07-17 15:24:23 +0200

    pull reservation

--- resources/views/reservationheures.blade.php
+++ resources/views/reservationheures.blade.php
@@ -97,19 +97,19 @@ function timeslots($duration,$cleanup, $start, $end){
 <div class="container mt-5" style="padding-top: 20px">
     <h1 class="text-center" style="font-size: 32px;font-weight: 700;text-transform: uppercase;">Horaire du
         <?php
-        setlocale (LC_TIME, 'fr_FR.utf8','fra'); 
+        setlocale (LC_TIME, 'fr_FR.utf8','fra');
         echo utf8_encode(ucwords(strftime('%A %d %B %G', strtotime($date))));
         ?></h1><hr>
     <div class="row">
         <div class="col-md-12 mt-3">
             <?php echo isset($msg)?$msg:""; ?>
         </div>
-        
+
         <?php $timeslots = timeslots($duration,$cleanup, $start, $end);
         $countH = 0;
         foreach($timeslots as $ts){
         ?>
-            <?php 
+            <?php
             if($countH == 0){
                 $countH = $countH +1;
             ?>
@@ -124,7 +124,7 @@ function timeslots($duration,$cleanup, $start, $end){
 
                         </div>
                     </div>
-        
+
             <?php
             }else if($countH != 4){
                 $countH = $countH +1;
@@ -141,7 +141,7 @@ function timeslots($duration,$cleanup, $start, $end){
                 </div>
             <?php
             }else if ($countH = 4){
-            
+
             ?>
                 </div>
                 <div class="col-md-3 mb-3">
@@ -155,12 +155,12 @@ function timeslots($duration,$cleanup, $start, $end){
 
                         </div>
                     </div>
-        
+
             <?php
                 $countH = 1;
             } ?>
         <?php } ?>
-    
+
 </div>
 </div>
 <div id="myModal" class="modal fade" role="dialog">
@@ -189,7 +189,7 @@ function timeslots($duration,$cleanup, $start, $end){
                                 <label for="">Nom</label>
                                 <input required type="text" class="form-control" name="name">
                             </div>
-                            
+
                             <div class="form-group">
                                 <label for="">Email</label>
                                 <input required type="email" class="form-control" name="email">
@@ -218,16 +218,16 @@ function timeslots($duration,$cleanup, $start, $end){
 {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>--}}
 {{--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>--}}
 <script>
-    
+
     $(".book").click(function(){
         var timeslot = $(this).attr('data-timeslot');
         $("#slot").html(timeslot);
         $("#timeslot").val(timeslot);
         $("#myModal").modal("show");
-        
+
     });
 
-    
+
 </script>
 </body>
 
